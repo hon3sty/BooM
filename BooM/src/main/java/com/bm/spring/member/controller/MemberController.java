bc94700afc32ba7807920d55c5ede347a4eaa4b3
hanori1 <ohhanel2219@naver.com>
2022-12-12 16:10:58 +0900

    MY_0030,PD_0030(장바구니,구매페이지) 작성 ->controller 작성 ->index-category란에 일단 링크만 연결 해놨음

--- BooM/src/main/java/com/bm/spring/member/controller/MemberController.java
+++ BooM/src/main/java/com/bm/spring/member/controller/MemberController.java
@@ -1,5 +1,16 @@
 package com.bm.spring.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MemberController {
+	
+	@RequestMapping("wishList.me")
+	public String wishList() {
+		return "mypage/MY_0030";
+	}
+	
+	
 
 }
