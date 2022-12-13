c0014fa37a150d0af4df3b2760ab1ed119041770
hanori1 <ohhanel2219@naver.com>
2022-12-13 13:32:55 +0900

    cart,order Vo/prductServiceImpl 작성

--- BooM/src/main/java/com/bm/spring/product/model/service/ProductServiceImpl.java
+++ BooM/src/main/java/com/bm/spring/product/model/service/ProductServiceImpl.java
@@ -1,5 +1,21 @@
 package com.bm.spring.product.model.service;
 
-public class ProductServiceImpl {
+import java.util.ArrayList;
 
+import com.bm.spring.product.model.vo.Cart;
+import com.bm.spring.product.model.vo.Order;
+
+public class ProductServiceImpl {
+	
+	//장바구니 목록
+	ArrayList<Cart> cartGetList;
+	
+	//상품 주문
+	int orderInsert;
+	
+	//주문 취소
+	int orderDelete;
+	
+	//구매 목록
+	ArrayList<Order> orderGetList;
 }
---
+++ BooM/src/main/java/com/bm/spring/product/model/vo/Cart.java
@@ -0,0 +1,15 @@
+package com.bm.spring.product.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Cart {
+	private int cartNo;
+	private int productNo;
+	private int memberNo;
+	private int cartCount;
+}
