dd1702b4cf436981aef4f0029f5b6a35490241d0
izzy1202 <izzy1202@gmail.com>
2022-12-13 16:36:48 +0900

    ㅌㅅㅌ

--- BooM/src/main/java/com/bm/spring/board/controller/BoardController.java
+++ BooM/src/main/java/com/bm/spring/board/controller/BoardController.java
@@ -5,58 +5,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
-	//고객센터 메인으로 이동
+	
+	// 고객센터 메인으로 이동
 	@RequestMapping("csMainForm.bo")
 	public String csMain() {
 		return "board/CS_0010";
 	}
-	//공지사항 작성 
+
+	// 공지사항 작성
 	public void noticeInsert() {
-		
+
 	}
-	
-	//공지사항 수정
+
+	// 공지사항 수정
 	public void noticeUpdate() {
-		
+
 	}
-	
-	//공지사항 삭제
+
+	// 공지사항 삭제
 	public void noticeDelete() {
-		
+
 	}
-	
-	//공지사항 게시글 리스트 조회
+
+	// 공지사항 게시글 리스트 조회
 	public void noticeSelectList() {
-		
+
 	}
-	
-	//공지사항 게시글 상세 조회
+
+	// 공지사항 게시글 상세 조회
 	public void noticeSelect() {
-		
+
 	}
-	
-	//FAQ 작성 
+
+	// FAQ 작성
 	public void faqInsert() {
-		
+
 	}
-	
-	//FAQ 수정
+
+	// FAQ 수정
 	public void faqUpdate() {
-		
+
 	}
-	
-	//FAQ 삭제
+
+	// FAQ 삭제
 	public void faqDelete() {
-		
+
 	}
-	
-	//FAQ 게시글 리스트 조회
+
+	// FAQ 게시글 리스트 조회
 	public void faqSelectList() {
-		
+
 	}
-	
-	//FAQ 게시글 상세 조회
+
+	// FAQ 게시글 상세 조회
 	public void faqSelect() {
-		
+
 	}
 }
